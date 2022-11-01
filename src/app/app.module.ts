5010ce0c428b553797aa937b81b10215e5c72107
verma-shivani <shivanipatel0020@gmail.com>
2022-11-02 02:58:04 +0530

    Learning project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrendingComponent } from './trending/trending.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrendingComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
